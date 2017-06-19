8352f5a6b9e8bb05027e6890e4e06f93ba89639c
tobias <thinkdifferent055@gmail.com>
2017-06-19 14:22:54 +0200

    Initial Commit

---
+++ Pacman3D-iOSTests/Pacman3D_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Pacman3D_iOSTests.swift
+//  Pacman3D-iOSTests
+//
+//  Created by Tobias on 19.06.17.
+//  Copyright © 2017 Tobias. All rights reserved.
+//
+
+import XCTest
+@testable import Pacman3D_iOS
+
+class Pacman3D_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
